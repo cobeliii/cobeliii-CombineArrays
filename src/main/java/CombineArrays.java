0d0549a4fe886dc2113a96f41d6864aa90231e6f
Newton Jorge <cobeliiimuname@gmail.com>
2024-08-26 23:53:43 +0000

    Completed Lab Combine Arrays

--- src/main/java/CombineArrays.java
+++ src/main/java/CombineArrays.java
@@ -8,7 +8,18 @@ public class CombineArrays {
      * @return an array containing the contents of arr1 followed by the contents of arr2.
      */
     public int[] combine(int[] arr1, int[] arr2){
+        int[] newArray = new int[arr1.length + arr2.length];
+        int x = 0;
+        for (int i = 0; i < arr1.length; i++) {
+            newArray[i] = arr1[i];
+            x++;
+        }
 
-        return null;
+        for(int i = 0; i < arr2.length; i++){
+            newArray[x] = arr2[i];
+            x++;
+        }
+
+        return newArray;
     }
 }
